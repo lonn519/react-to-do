3f06d75edb15874868f3ff1721529cbd2a185580
Lonn Lee <lonn.lee@gmail.com>
2018-09-21 13:17:53 -0700

    Assignment M2React-08 Completed. Added Delete button.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,7 +6,8 @@ class ToDo extends Component {
             <li>
                 <input type="checkbox" checked={this.props.isCompleted} onChange = {this.props.toggleComplete} />
                 <span>{ this.props.description }</span>
-                </li>
+                <input type="submit" value='delete' onClick = {this.props.deleteTodo} />
+            </li>
         );
     }
 }
